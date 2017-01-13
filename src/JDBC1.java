c9fe522afd63282b50fbf3d5964c7988a9c062b2
Zijia Ke <kevinkozj@gmail.com>
2017-01-13 10:59:37 -0800

    Update some UI

---
+++ src/JDBC1.java
@@ -0,0 +1,20 @@
+// JDBC Example - printing a database's metadata
+// Coded by Chen Li/Kirill Petrov Winter, 2005
+// Slightly revised for ICS185 Spring 2005, by Norman Jacobson
+
+
+import java.sql.*;                              // Enable SQL processing
+
+public class JDBC1
+{
+	
+       public static void main(String[] arg) throws Exception
+       {
+    	   MovieDBConsole console = new MovieDBConsole();
+
+           console.connect();
+           console.printMoviesWithStars();
+           console.insertNewStar();
+           console.insertNewCustomer();
+       }
+}
