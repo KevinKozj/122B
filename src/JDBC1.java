48fd562250dec385b02a121f7cb1c8b8203d2eba
ruidongd <ruidongd@outlook.com>
2017-01-13 11:04:10 -0800

    add hello world

--- src/JDBC1.java
+++ src/JDBC1.java
@@ -1,13 +1,13 @@
 // JDBC Example - printing a database's metadata
 // Coded by Chen Li/Kirill Petrov Winter, 2005
 // Slightly revised for ICS185 Spring 2005, by Norman Jacobson
-
+"Hello World";
 
 import java.sql.*;                              // Enable SQL processing
 
 public class JDBC1
 {
-	
+
        public static void main(String[] arg) throws Exception
        {
     	   MovieDBConsole console = new MovieDBConsole();
